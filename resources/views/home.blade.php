da23b38ba252d9cc4c29b684e53d66208da01559
ahmadahsana <ahmadahsanataqwim@gmail.com>
2022-08-08 15:01:40 +0700

    yoyoyo

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.main')
+
+@section('container')
+    fsdgsdgsd
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/home', function () {
+    return view('home', [
+        'title' => 'Home'
+    ]);
+});
+Route::get('/about', function () {
+    return view('about', [
+        'title' => 'About'
+    ]);
+});
+Route::get('/posts', function () {
+    return view('posts', [
+        'title' => 'posts'
+    ]);
 });
